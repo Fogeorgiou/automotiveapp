e8953654470b0947ec6731742ce012e54ec347b1
fotin <fotin@192.168.178.13>
2020-01-24 20:55:16 +0100

    Creation of Spring Boot application + initial setup around the db connection and the server port

---
+++ src/main/java/com/adesso/demo/automotiveapp/AutomotiveappApplication.java
@@ -0,0 +1,13 @@
+package com.adesso.demo.automotiveapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomotiveappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomotiveappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adesso/demo/automotiveapp/AutomotiveappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adesso.demo.automotiveapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomotiveappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
